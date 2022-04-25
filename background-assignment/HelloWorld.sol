fe0bdd53f244c8b0ff37e57d965bb04786f69b3b
Youssef Agiza <youssefagiza@aucegypt.edu>
2022-04-25 13:49:58 +0200

    adds HelloWorld.sol

---
+++ background-assignment/HelloWorld.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+contract HelloWorld{
+    int public number;    
+
+    function storeNumber(int n)external{
+        number=n;
+    }
+    function retrieveNumber()external view returns(int) {
+        return number;
+    }
+}
